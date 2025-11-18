0cbcd57ec22907ab689dc726e20baffe323033e3
KiszelPetra <kiszel.petra@googlemail.com>
2025-11-19 00:35:29 +0100

    Hielo beillesztés Laravelbe

--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,19 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('welcome');
-})->name('home');
+//Route::get('/', function () {
+//    return Inertia::render('welcome');
+//})->name('home');
 
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('dashboard', function () {
-        return Inertia::render('dashboard');
-    })->name('dashboard');
-});
+//Route::middleware(['auth', 'verified'])->group(function () {
+//    Route::get('dashboard', function () {
+//        return Inertia::render('dashboard');
+//   })->name('dashboard');
+//});
+
+Route::view('/', 'index');
+Route::view('/generic', 'generic');
+Route::view('/elements', 'elements');
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
