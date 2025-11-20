e5c1fedea2fbc13d891f1472c68ba92398b628af
KiszelPetra <kiszel.petra@googlemail.com>
2025-11-20 12:15:09 +0100

    Saját Hielo + Autentikáció
    Hielo templátba saját tanösvényes téma bemutatása
    Menü pontban: Laravel autentikáció: Login/Register

--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,16 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-//Route::get('/', function () {
-//    return Inertia::render('welcome');
-//})->name('home');
+Route::view('/', 'index')->name('home');
 
-//Route::middleware(['auth', 'verified'])->group(function () {
-//    Route::get('dashboard', function () {
-//        return Inertia::render('dashboard');
-//   })->name('dashboard');
-//});
 
-Route::view('/', 'index');
+Route::middleware(['auth', 'verified'])->group(function () {
+Route::get('dashboard', function () {
+return Inertia::render('dashboard');
+})->name('dashboard');
+});
+
+
 Route::view('/generic', 'generic');
 Route::view('/elements', 'elements');
 
